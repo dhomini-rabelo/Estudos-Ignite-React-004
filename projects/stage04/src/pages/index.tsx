ec758e8634b5453360f13908fdb39ff2de041bc2
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-12-18 22:59:33 -0300

    add carrousel

--- projects/stage04/src/pages/index.tsx
+++ projects/stage04/src/pages/index.tsx
@@ -1,17 +1,47 @@
 import { A, Div } from "../layout/styles/pages/home";
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <Div.container className="flex gap-12 text-white w-full ml-auto">
-      <A.product href="/" className="rounded-lg p-1 flex items-center justify-center">
+    <Div.container className="flex text-white w-full ml-auto keen-slider" ref={sliderRef}>
+      <A.product href="/" className="rounded-lg p-1 flex items-center justify-center keen-slider__slide">
         <img src="/camisas/camisa01.png" alt="t-shirt-01" />
         <footer className="flex items-center justify-between">
           <strong className="text-clg">Camisa 00</strong>
           <span className="text-cxl font-bold text-Green-300">R$ 80,00</span>
         </footer>
       </A.product>
-      <A.product href="/" className="rounded-lg p-1 flex items-center justify-center">
-        <img src="/camisas/camisa01.png" alt="t-shirt-01" />
+      <A.product href="/" className="rounded-lg p-1 flex items-center justify-center keen-slider__slide">
+        <img src="/camisas/camisa02.png" alt="t-shirt-01" />
+        <footer className="flex items-center justify-between">
+          <strong className="text-clg">Camisa 00</strong>
+          <span className="text-cxl font-bold text-Green-300">R$ 80,00</span>
+        </footer>
+      </A.product>
+      <A.product href="/" className="rounded-lg p-1 flex items-center justify-center keen-slider__slide">
+        <img src="/camisas/camisa03.png" alt="t-shirt-01" />
+        <footer className="flex items-center justify-between">
+          <strong className="text-clg">Camisa 00</strong>
+          <span className="text-cxl font-bold text-Green-300">R$ 80,00</span>
+        </footer>
+      </A.product>
+      <A.product href="/" className="rounded-lg p-1 flex items-center justify-center keen-slider__slide">
+        <img src="/camisas/camisa03.png" alt="t-shirt-01" />
+        <footer className="flex items-center justify-between">
+          <strong className="text-clg">Camisa 00</strong>
+          <span className="text-cxl font-bold text-Green-300">R$ 80,00</span>
+        </footer>
+      </A.product>
+      <A.product href="/" className="rounded-lg p-1 flex items-center justify-center keen-slider__slide">
+        <img src="/camisas/camisa03.png" alt="t-shirt-01" />
         <footer className="flex items-center justify-between">
           <strong className="text-clg">Camisa 00</strong>
           <span className="text-cxl font-bold text-Green-300">R$ 80,00</span>
