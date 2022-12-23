57e29483ab55b546f901a0aaecf6889944ea63f1
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-12-22 23:22:59 -0300

    add content for success page

---
+++ projects/stage04/src/layout/styles/pages/sucesso.ts
@@ -0,0 +1,10 @@
+import { styled } from "..";
+
+export const Div = {
+  imageContainer: styled('div', {
+    background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+    img: {
+      objectFit: 'cover',
+    }
+  })
+}
--- projects/stage04/src/pages/sucesso.tsx
+++ projects/stage04/src/pages/sucesso.tsx
@@ -1,6 +1,20 @@
+import Link from "next/link";
+import { Div } from "../layout/styles/pages/sucesso";
+
 export default function SuccessfulPayment() {
   return (
-    <h1>Sucesso</h1>
+    <main className="flex flex-col items-center justify-center mx-auto h-[656px]">
+      <h1 className="text-c2xl text-Gray-100 font-bold">Compra efetuada!</h1>
+      <Div.imageContainer className="w-full max-w-[130px] h-[145px] rounded-lg p-1 flex items-center justify-center mt-16">
+        <img src="/camisas/camisa01.png" alt="" />
+      </Div.imageContainer>
+      <p className="text-cxl text-Gray-300 max-w-[560px] text-center mt-8 lh-130">
+        Uhuul, sua compra já está a caminho da sua casa.
+      </p>
+      <Link href="/" className="mt-20 block text-Green-500 text-clg hover:text-Green-300 font-bold">
+        Voltar ao catálogo
+      </Link>
+    </main>
   )
 }
 
