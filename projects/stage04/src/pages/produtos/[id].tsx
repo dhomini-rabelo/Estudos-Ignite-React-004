e126ca591b574288df3512ddd05269d761d5bf62
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-12-23 23:58:00 -0300

    use head tag

--- projects/stage04/src/pages/produtos/[id].tsx
+++ projects/stage04/src/pages/produtos/[id].tsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { GetStaticPaths, GetStaticProps } from "next"
+import Head from "next/head"
 import { useRouter } from "next/router"
 import { useState } from "react"
 import Stripe from "stripe"
@@ -43,21 +44,26 @@ export default function Product({ product }: Props) {
   }
 
   return (
-    <main className="grid grid-cols-2 items-stretch gap-16 max-w-[1180px] mx-auto">
-      <Div.imageContainer className="col-span-1 w-full max-w-[576px] rounded-lg p-1 flex items-center justify-center">
-        <img src={product.imageUrl} alt="product-image" />
-      </Div.imageContainer>
-      <div className="col-span-1 flex flex-col">
-        <h1 className="text-c2xl text-Gray-300">{product.name}</h1>
-        <span className="mt-4 block text-c2xl text-Gray-300">{product.price}</span>
-        <p className="mt-10 text-cmd lh-160 text-Gray-300">{product.description}</p>
-        <button onClick={handleBuy} disabled={isCreatingCheckoutSession}
-          className="mt-auto bg-Green-500 border-0 text-White rounded-lg p-5 cursor-pointer font-bold text-cmd hover:bg-Green-300 disabled:opacity-60 disabled:cursor-not-allowed disabled:hover:bg-Green-500"
-        >
-          Comprar agora
-        </button>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+      <main className="grid grid-cols-2 items-stretch gap-16 max-w-[1180px] mx-auto">
+        <Div.imageContainer className="col-span-1 w-full max-w-[576px] rounded-lg p-1 flex items-center justify-center">
+          <img src={product.imageUrl} alt="product-image" />
+        </Div.imageContainer>
+        <div className="col-span-1 flex flex-col">
+          <h1 className="text-c2xl text-Gray-300">{product.name}</h1>
+          <span className="mt-4 block text-c2xl text-Gray-300">{product.price}</span>
+          <p className="mt-10 text-cmd lh-160 text-Gray-300">{product.description}</p>
+          <button onClick={handleBuy} disabled={isCreatingCheckoutSession}
+            className="mt-auto bg-Green-500 border-0 text-White rounded-lg p-5 cursor-pointer font-bold text-cmd hover:bg-Green-300 disabled:opacity-60 disabled:cursor-not-allowed disabled:hover:bg-Green-500"
+          >
+            Comprar agora
+          </button>
+        </div>
+      </main>
+    </>
   )
 }
 
--- projects/stage04/src/pages/sucesso.tsx
+++ projects/stage04/src/pages/sucesso.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from "next";
 import Link from "next/link";
+import Head from "next/head"
 import Stripe from "stripe";
 import { stripe } from "../code/services";
 import { Div } from "../layout/styles/pages/sucesso";
@@ -16,22 +17,28 @@ interface Props {
 
 export default function SuccessfulPayment({ customer, product }: Props) {
   return (
-    <main className="flex flex-col items-center justify-center mx-auto h-[656px]">
-      <h1 className="text-c2xl text-Gray-100 font-bold">Compra efetuada!</h1>
-      <Div.imageContainer className="w-full max-w-[130px] h-[145px] rounded-lg p-1 flex items-center justify-center mt-16">
-        <img src={product ? product.imageUrl : '/camisas/camisa01.png'} alt="product-image" />
-      </Div.imageContainer>
-      <p className="text-cxl text-Gray-300 max-w-[560px] text-center mt-8 lh-130">
-        {(product && customer) ? (
-          <>Uhuul <strong>{customer}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.</>
-        ) : (
-          <>Uhuul, sua compra já está a caminho da sua casa.</>
-        )}
-      </p>
-      <Link href="/" className="mt-20 block text-Green-500 text-clg hover:text-Green-300 font-bold">
-        Voltar ao catálogo
-      </Link>
-    </main>
+    <>
+      <Head>
+        <title>Sucesso | Ignite Shop</title>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <main className="flex flex-col items-center justify-center mx-auto h-[656px]">
+        <h1 className="text-c2xl text-Gray-100 font-bold">Compra efetuada!</h1>
+        <Div.imageContainer className="w-full max-w-[130px] h-[145px] rounded-lg p-1 flex items-center justify-center mt-16">
+          <img src={product ? product.imageUrl : '/camisas/camisa01.png'} alt="product-image" />
+        </Div.imageContainer>
+        <p className="text-cxl text-Gray-300 max-w-[560px] text-center mt-8 lh-130">
+          {(product && customer) ? (
+            <>Uhuul <strong>{customer}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.</>
+          ) : (
+            <>Uhuul, sua compra já está a caminho da sua casa.</>
+          )}
+        </p>
+        <Link href="/" className="mt-20 block text-Green-500 text-clg hover:text-Green-300 font-bold">
+          Voltar ao catálogo
+        </Link>
+      </main>
+    </>
   )
 }
 
