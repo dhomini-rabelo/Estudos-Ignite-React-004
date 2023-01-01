565ef46056df59cdef72e6ccae3ca3b1a37c6b2b
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-01 11:36:51 -0300

    using CartContext

--- projects/practice04/src/pages/_app.tsx
+++ projects/practice04/src/pages/_app.tsx
@@ -4,34 +4,48 @@ import { globalStyles } from '../layout/styles/global'
 import '../layout/styles/globalCss.css'
 import Head from 'next/head'
 import { Handbag } from 'phosphor-react'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { Cart } from '../layout/components/Cart'
+import { CartContext, CartProvider } from '../code/contexts/cart'
 
 globalStyles()
 
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { data } = useContext(CartContext)
+  const hasProductsInTheCart = data && data.products.length > 0
   const [cartViewIsOpened, setCartViewIsOpened] = useState(false)
-  const openCartView = () => setCartViewIsOpened(true)
+  const openCartView = () => hasProductsInTheCart && setCartViewIsOpened(true)
   const closeCartView = () => setCartViewIsOpened(false)
 
 
   return (
     <>
-      <div id="root" className="flex flex-col items-start justify-center min-h-[100vh]">
-        <header className="py-8 px-0 w-full max-w-[1180px] mx-auto flex items-center justify-between">
-          <Link href="/">
-            <img src="/global/logo.svg" alt="project-logo" />
-          </Link>
-          <button className="text-Gray-500 p-3 font-bold" onClick={openCartView}>
-            <Handbag size={24} />
-          </button>
-        </header>
-        <Component {...pageProps} />
-      </div>
-      {cartViewIsOpened && (
-        <Cart handleClose={closeCartView} />
-      )}
+      <CartProvider>
+        <div id="root" className="flex flex-col items-start justify-center min-h-[100vh]">
+          <header className="py-8 px-0 w-full max-w-[1180px] mx-auto flex items-center justify-between">
+            <Link href="/">
+              <img src="/global/logo.svg" alt="project-logo" />
+            </Link>
+            {hasProductsInTheCart ? (
+              <button className="text-Gray-100 p-3 font-bold relative" onClick={openCartView}>
+                <div className="w-6 h-6 bg-Green-500 absolute text-center rounded-full -top-2 -right-2">
+                  <strong className="text-sm text-White lh-160">1</strong>
+                </div>
+                <Handbag size={24} />
+              </button>
+            ) : (
+              <button className="text-Gray-500 p-3 font-bold cursor-not-allowed">
+                <Handbag size={24} />
+              </button>
+            )}
+          </header>
+          <Component {...pageProps} />
+        </div>
+        {cartViewIsOpened && (
+          <Cart handleClose={closeCartView} />
+        )}
+      </CartProvider>
     </>
   )
 }
