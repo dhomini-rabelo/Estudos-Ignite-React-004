c32d4adf2eebbb0ebe50834c921ef3a521943a32
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-01 21:20:38 -0300

    adapting checkout for many products

--- projects/practice04/src/pages/api/checkout.ts
+++ projects/practice04/src/pages/api/checkout.ts
@@ -1,34 +1,66 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { ProductSchemaType } from "../../code/schemas/products";
 import { stripe } from "../../code/services";
 import { PROJECT_URL } from "../../code/settings";
 
+interface BodyType {
+  products: ProductSchemaType[]
+  cancelUrl?: string
+}
+
+interface ErrorsType { [field: string]: string }
+
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { priceId, productId } = req.body
-  if (req.method !== 'POST') {
-    return res.status(405)
-  } else if (!priceId) {
-    return res.status(400).json({
-      errors: {
-        priceId: 'This field is required'
+
+  function validateBody({ products }: BodyType) {
+    const errors: ErrorsType[] = []
+    products.forEach(product => {
+      const itemError: ErrorsType = {}
+      if (!product.priceId) {
+        itemError.priceId = 'This field is required'
+      }
+      if (Object.keys(itemError).length > 0) {
+        errors.push(itemError)
       }
     })
-  } else if (!productId) {
+    if (Object.keys(errors).length > 0) {
+      return {
+        isValid: false,
+        errors: errors,
+      }
+    } else {
+      return {
+        isValid: true,
+        errors: null,
+      }
+    }
+  }
+
+  const body: BodyType = req.body
+  if (req.method !== 'POST') {
+    return res.status(405)
+  } else if (!body.products || body.products.length === 0) {
     return res.status(400).json({
       errors: {
-        productId: 'This field is required'
+        products: 'This field is required'
       }
     })
   }
+
+  const validationProcess = validateBody(body)
+
+  if (!validationProcess.isValid) {
+    return res.status(400).json({
+      errors: validationProcess.errors
+    })
+  }
+
   const checkoutSession = await stripe.checkout.sessions.create({
     mode: 'payment',
-    line_items: [
-      {
-        price: priceId,
-        quantity: 1,
-      }
-    ],
+    line_items: body.products.map(product => ({ price: product.priceId, quantity: 1 })),
     success_url: `${PROJECT_URL}/sucesso?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${PROJECT_URL}/produtos/${productId}`,
+    cancel_url: body.cancelUrl || PROJECT_URL,
   })
   return res.status(201).json({
     checkoutUrl: checkoutSession.url,
